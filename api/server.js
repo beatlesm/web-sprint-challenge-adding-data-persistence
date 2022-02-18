10bc45eceb950264b609b00ebabf07413aa4b97d
beatlesm <rj.geng@gmail.com>
2022-02-18 06:42:05 -0800

    Required Dependencies and Necessary codes to index.js and api/server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,31 @@
-// build your server here and require it from index.js
+const express = require('express');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors')
+
+const projectsRouter = require('../api/project/router') 
+const resourcesRouter = require('../api/resource/router') 
+const tasksRouter = require('../api/task/router') 
+
+const server = express();
+server.use(express.json());
+server.use(logger('dev'));
+server.use(helmet());
+server.use(cors()) // cors() returns a (req, res, nex) => { // stuff and then next() }
+
+server.use('/api/projects', projectsRouter)
+server.use('/api/resources', resourcesRouter)
+server.use('/api/tasks', tasksRouter)
+
+server.use('/', (req, res)=>{
+    res.send('hello!')
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line   
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// start your server here
+const server = require('./api/server')
+const { PORT } = require('./secret')
+
+server.listen(PORT, () => {
+  console.log(`\n** Running on port ${PORT} **\n`)
+})
