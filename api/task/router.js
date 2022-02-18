8997aa882934c831a60676cad5936c25823e4946
beatlesm <rj.geng@gmail.com>
2022-02-18 08:42:29 -0800

    Required Endpoints - [GET] /api/tasks

--- api/task/router.js
+++ api/task/router.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 const helpers = require('./model');
 
+router.get('/', (req, res, next) => {
+    helpers.find()
+      .then(task => {
+        res.json(task)
+      })
+      .catch(next)
+  })
+
+
+
 
 module.exports = router;
