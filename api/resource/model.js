fd48e735af6ded516e2434b6818b000a186821d7
beatlesm <rj.geng@gmail.com>
2022-02-18 08:27:52 -0800

    Required Endpoints - [GET] /api/resources

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,20 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+async function find() {
+    return db('resources');
+}
+
+function findById(id) {
+    return db('resources').where('resource_id', id).first();
+  }
+
+async function add (resource) {
+    const [id] = await db('resources').insert(resource);
+    return findById(id);
+  }
+
+module.exports = {
+  find,
+  findById,
+  add
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 const helpers = require('./model');
 
+router.get('/', (req, res, next) => {
+    helpers.find()
+      .then(resource => {
+        res.json(resource)
+      })
+      .catch(next)
+  })
+
+
+
 
 module.exports = router;
