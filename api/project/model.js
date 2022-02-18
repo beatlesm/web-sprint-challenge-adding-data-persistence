af473dba32887895643fddf6f2910f2f4eb50f35
beatlesm <rj.geng@gmail.com>
2022-02-18 08:12:17 -0800

    Required Endpoints - [GET] /api/projects

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,34 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+async function find() {
+
+    const rows = await db('projects')
+      .select('*')
+    const result =  rows.map (row => {      
+      if (row.project_completed) row.project_completed = true  
+           else row.project_completed = false
+      return row
+    })
+    return result
+
+}
+
+async function findById(id) {
+    // return db('projects').where('project_id', id).first();
+
+    const row = await  db('projects').where('project_id', id).first()
+      if (row.project_completed) row.project_completed = true  
+      else row.project_completed = false
+    return row
+  }
+
+async function add (project) {
+    const [id] = await db('projects').insert(project);
+    return findById(id);
+  }
+
+module.exports = {
+  find,
+  findById,
+  add
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 const helpers = require('./model');
 
+router.get('/', (req, res, next) => {
+    helpers.find()
+      .then(project => {
+        res.json(project)
+      })
+      .catch(next)
+  })
+
+
+
 
 module.exports = router;
